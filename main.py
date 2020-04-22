52c2e60f69ed92127add00a3076436fdd7478ea5
Кирилл Рыхлинский <kirill2006.ryhl@gmail.com>
2020-04-22 19:10:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,7 @@
-from io import StringIO
-import sys
-import traceback
+import pymysql
+import pymysql.cursors
 
-class OutputInterceptor(list):
-    def __enter__(self):
-        self._stdout = sys.stdout
-        sys.stdout = self._stringio = StringIO()
-        return self
-
-    def __exit__(self, *args):
-        self.extend(self._stringio.getvalue().splitlines())
-        del self._stringio
-        sys.stdout = self._stdout
-
-
-def ListenOutput(code):
-    with OutputInterceptor() as output:
-        try:
-            eval(code)
-        except Exception as e:
-            print(traceback.format_exc())
-    return str(output)
-
-while True:
-    print('!: '+ListenOutput(input(': ')))
+def conn():
+    # con = pymysql.connect('localhost', 'root', '', 'minecraft',cursorclass=pymysql.cursors.DictCursor)
+    con = pymysql.connect('node84046-empty.mircloud.ru', 'root', 'VSNrrr74711', 'Empty',cursorclass=pymysql.cursors.DictCursor)
+    return con
